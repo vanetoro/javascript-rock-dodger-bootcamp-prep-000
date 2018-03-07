702b1c078ed024da22179e265d4f96719d3c9590
Vanessa Toro <vanetoro84@gmail.com>
2018-03-07 22:52:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -158,6 +158,7 @@ function moveDodger(e) {
    */
      var pos = e.which;
      if(pos === LEFT_ARROW){
+       event.preventDefault();
        moveDodgerLeft();
      } else
      if (pos === RIGHT_ARROW){
